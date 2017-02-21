79aadb1f6b603e65b6c87deffebbeacc26290d39
jos <mail@jos.tw>
2017-02-22 00:58:03 +0800

    Update test suites with multiple tests

--- src/components/HeroProfile.test.js
+++ src/components/HeroProfile.test.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import HeroProfile from './HeroProfile';
 
 it('renders without crashing', () => {
-  const profile = {
-    str: Math.floor(Math.random() * 100),
-    int: Math.floor(Math.random() * 100),
-    agi: Math.floor(Math.random() * 100),
-    luk: Math.floor(Math.random() * 100),
-    remainder: Math.floor(Math.random() * 100),
-    increaseHeroStats: jest.fn(),
-    decreaseHeroStats: jest.fn(),
-    updateHeroProfile: jest.fn()
-  };
+  const profile = createMockProfile();
 
-  const wrapper = mount(
+  const wrapper = shallow(
     <HeroProfile { ...profile } />
   );
 
@@ -25,6 +16,28 @@ it('renders without crashing', () => {
   expect(wrapper.find('.hero-profile-remainder').length).toBe(1);
   expect(wrapper.find('.hero-profile-remainder').text()).toBe(`剩餘點數：${profile.remainder}`);
   expect(wrapper.find('.hero-profile-save').length).toBe(1);
+});
+
+it('update hero profile on saving', () => {
+  const profile = createMockProfile();
+
+  const wrapper = mount(
+    <HeroProfile { ...profile } />
+  );
+
   wrapper.find('.hero-profile-save').simulate('click');
   expect(wrapper.props().updateHeroProfile.mock.calls.length).toBe(1);
 });
+
+function createMockProfile() {
+  return {
+    str: Math.floor(Math.random() * 100),
+    int: Math.floor(Math.random() * 100),
+    agi: Math.floor(Math.random() * 100),
+    luk: Math.floor(Math.random() * 100),
+    remainder: Math.floor(Math.random() * 100),
+    increaseHeroStats: jest.fn(),
+    decreaseHeroStats: jest.fn(),
+    updateHeroProfile: jest.fn()
+  };
+}
--- src/components/HeroStats.test.js
+++ src/components/HeroStats.test.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import HeroStats from './HeroStats';
 
 it('renders without crashing', () => {
-  const stats = {
-    label: 'test',
-    value: Math.floor(Math.random() * 100),
-    remainder: Math.floor(Math.random() * 100),
-    increaseHeroStats: jest.fn(),
-    decreaseHeroStats: jest.fn()
-  };
+  const stats = createMockStats();
 
   const wrapper = mount(
     <HeroStats { ...stats } />
@@ -21,6 +15,14 @@ it('renders without crashing', () => {
   expect(wrapper.find('.hero-stats-label').text()).toBe(stats.label.toUpperCase());
   expect(wrapper.find('.hero-stats-input').length).toBe(1);
   expect(wrapper.find('.hero-stats-input').getNode().value).toBe(`${stats.value}`);
+});
+
+it('increase and decrease hero stats', () => {
+  const stats = createMockStats();
+
+  const wrapper = mount(
+    <HeroStats { ...stats } />
+  );
 
   expect(wrapper.find('.hero-stats-increase').length).toBe(1);
   wrapper.find('.hero-stats-increase').simulate('click');
@@ -29,6 +31,14 @@ it('renders without crashing', () => {
   expect(wrapper.find('.hero-stats-decrease').length).toBe(1);
   wrapper.find('.hero-stats-decrease').simulate('click');
   expect(wrapper.props().decreaseHeroStats.mock.calls.length).toBe(1);
+});
+
+it('disabled when specified value is 0', () => {
+  const stats = createMockStats();
+
+  const wrapper = shallow(
+    <HeroStats { ...stats } />
+  );
 
   wrapper.setProps({ remainder: 0 });
   expect(wrapper.find('.hero-stats-increase-disabled').length).toBe(1);
@@ -36,3 +46,13 @@ it('renders without crashing', () => {
   wrapper.setProps({ value: 0 });
   expect(wrapper.find('.hero-stats-decrease-disabled').length).toBe(1);
 });
+
+function createMockStats() {
+  return {
+    label: 'test',
+    value: Math.floor(Math.random() * 100),
+    remainder: Math.floor(Math.random() * 100),
+    increaseHeroStats: jest.fn(),
+    decreaseHeroStats: jest.fn()
+  };
+}
